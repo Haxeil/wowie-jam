455a83a8a0a81acde4a0f2eb0aa72d33dacc30bc
shanks-red <lezharayman@gmail.com>
2021-02-26 22:37:43 +0100

    player controller template

--- Scripts/controller.gd
+++ Scripts/controller.gd
@@ -15,14 +15,14 @@ func movement() -> void:
 	elif Input.is_action_pressed("Down") and not Input.is_action_pressed("Up"):
 		player_velocity.y = 1
 	else:
-		player_velocity.y = 0
+		player_velocity.y = lerp(0, player_velocity.y, player.friction)
 	
 	if Input.is_action_pressed("Right") and not Input.is_action_pressed("Left"):
 		player_velocity.x = 1
 	elif Input.is_action_pressed("Left") and not Input.is_action_pressed("Right"):
 		player_velocity.x = -1
 	else:
-		player_velocity.x = 0
+		player_velocity.x = lerp(0, player_velocity.x, player.friction)
 
 	player.move_and_slide(player_velocity.normalized() * player.SPEED);
 
